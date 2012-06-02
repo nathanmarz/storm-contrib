cd94603b5105b11f1ab13d2e4133743e019c2e88
Michael Cetrulo <mail2samus@gmail.com>
2012-06-02 14:20:01 -0300

    initialized lein project

---
+++ storm-ml/project.clj
@@ -0,0 +1,9 @@
+(defproject storm-ml "0.0.1-SNAPSHOT"
+  :description "Online machine learning library on top of Storm"
+  :url "https://github.com/nathanmarz/storm-contrib/tree/master/storm-ml"
+  :source-path "src/clj"
+  :java-source-path "src/jvm"
+  :javac-options {:debug "true" :fork "true"}
+  :aot :all
+  :dev-dependencies [[org.clojure/clojure "1.4.0"]
+                     [storm "0.7.2"]])
