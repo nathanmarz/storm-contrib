7b5f9322c2ab2c7d013165f3635748392bb0178f
Michael Cetrulo <mail2samus@gmail.com>
2012-07-02 03:33:49 -0300

    parse input

--- storm-ml/src/jvm/storm/ml/bolt/EvaluationBolt.java
+++ storm-ml/src/jvm/storm/ml/bolt/EvaluationBolt.java
@@ -8,14 +8,24 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.List;
 
 public class EvaluationBolt extends BaseBasicBolt {
     @Override
     public void execute(Tuple tuple, BasicOutputCollector collector) {
+        List<Double> weights = new ArrayList<Double>();
+        weights.add(1.0);
+        weights.add(2.0);
+
+        String input = tuple.getString(1);
         List<Double> result = new ArrayList<Double>();
-        result.add(0.0);
-        result.add(0.0);
+
+        Scanner scanner = new Scanner(input.substring(1, input.length()-1));
+        scanner.useDelimiter(", ");
+
+        while (scanner.hasNextDouble())
+            result.add(scanner.nextDouble());
 
         collector.emit(new Values(tuple.getValue(0), result));
     }
