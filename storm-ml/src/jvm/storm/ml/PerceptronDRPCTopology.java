e5b1aa839985054b9c29bf731c6f2f8c894cd2fe
Michael Cetrulo <mail2samus@gmail.com>
2012-07-17 07:04:01 -0300

    show error %

--- storm-ml/src/jvm/storm/ml/PerceptronDRPCTopology.java
+++ storm-ml/src/jvm/storm/ml/PerceptronDRPCTopology.java
@@ -47,16 +47,24 @@ public class PerceptronDRPCTopology {
             cluster.submitTopology("training-demo", conf, builder.createTopology());
             cluster.submitTopology("evaluation-demo", conf, drpc_builder.createLocalTopology(drpc));
 
+            int error_count = 0;
+
             List<String> input_vectors = get_input_vectors();
-            for (String input_vector : input_vectors) {
+            for (int i=0; i<input_vectors.size(); i++) {
+                String input_vector = input_vectors.get(i);
+
                 List<Double> parsed_iv = Util.parse_str_vector(input_vector);
                 Double x = parsed_iv.get(0);
                 Double y = parsed_iv.get(1);
 
                 String result = drpc.execute("evaluate", input_vector);
-                int expected_result = TrainingSpout.get_label(x, y);
+                Integer expected_result = TrainingSpout.get_label(x, y);
 
-                System.out.println(String.format("%s -> %s (expected: %s)", input_vector, result, expected_result));
+                if (!result.equals(expected_result.toString()))
+                    error_count += 1;
+
+                String output_str = String.format("%s -> %s (expected: %s, %.2f%% error)", input_vector, result, expected_result, 100*error_count/Double.valueOf(i+1));
+                System.out.println(output_str);
             }
 
             cluster.shutdown();
