9b90736084d2c0a2a421052b6d761115e3cb78c6
Jason Jackson <jackson@twitter.com>
2012-08-30 14:44:59 -0700

    upgrade to leiningen2

--- storm-kafka/project.clj
+++ storm-kafka/project.clj
@@ -1,13 +1,14 @@
-(defproject storm/storm-kafka "0.8.0-wip3"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :javac-options {:debug "true" :fork "true"}
-  :repositories {"releases" "http://artifactory.local.twitter.com/libs-releases-local"
-                 "snapshots" "http://artifactory.local.twitter.com/libs-snapshots-local"}
+(defproject storm/storm-kafka "0.8.0-wip4" 
+  :url "https://github.com/nathanmarz/storm-contrib/tree/master/storm-kafka"
+  :description "Kafka support for Storm"
+  :license {:url "Eclipse Public License - Version 1.0"}
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
   :dependencies [[org.clojars.jasonjckn/kafka_2.9.1 "0.7.0"
-                   :exclusions [org.apache.zookeeper/zookeeper
-                                log4j/log4j]]]
-  :dev-dependencies [[storm "0.8.0"]
-                     [org.clojure/clojure "1.4.0"]]
-  :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
-)
+                  :exclusions [org.apache.zookeeper/zookeeper log4j/log4j]]]
+                  
+  :profiles {:provided {:dependencies [[storm "0.8.0"] [org.clojure/clojure "1.4.0"]]}}
+
+  :min-lein-version "2.0.0"
+  :javac-options {:debug "true"}
+  :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"])
