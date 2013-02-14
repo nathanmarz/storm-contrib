d3d913183e1f047e999146be3ffec1bf733485ce
P. Taylor Goetz <ptgoetz@gmail.com>
2013-02-13 20:40:45 -0500

    convert storm-cassandra, storm-jms, and storm-signals to git submodules

--- storm-cassandra/src/main/java/backtype/storm/contrib/cassandra/bolt/serialize/CassandraSerializer.java
+++
@@ -1,7 +0,0 @@
-package backtype.storm.contrib.cassandra.bolt.serialize;
-
-import backtype.storm.tuple.Tuple;
-
-public interface CassandraSerializer {
-	public void writeToCassandra(Tuple tuple);
-}
