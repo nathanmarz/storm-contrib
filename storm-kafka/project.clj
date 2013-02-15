d62d9c45ee706e006858951503b6c9160fe8ac17
agallego <gallego.alexx@gmail.com>
2013-02-14 21:26:47 -0500

    only version 0.8.2

--- storm-kafka/project.clj
+++ storm-kafka/project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-kafka "0.8.0-wip2"
+(defproject storm/storm-kafka "0.8.2"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
@@ -7,7 +7,7 @@
   :dependencies [[storm/kafka "0.7.0-incubating"
                    :exclusions [org.apache.zookeeper/zookeeper
                                 log4j/log4j]]]
-  :dev-dependencies [[storm "0.8.0"]
+  :dev-dependencies [[storm "0.8.2"]
                      [org.clojure/clojure "1.4.0"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )
