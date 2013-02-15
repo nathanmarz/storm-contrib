ba748622de4025a93841c8fe8fba3c70bfe31595
agallego <gallego.alexx@gmail.com>
2013-02-15 14:23:17 -0500

    the dependency on the kafka release download page is 2.8.0 when you look at the sbt file that comes package with it. so I set this dependency explicitly
    This is a working branch for 0.8.2

--- storm-kafka/project.clj
+++ storm-kafka/project.clj
@@ -4,36 +4,21 @@
   :javac-options {:debug "true" :fork "true"}
   :repositories {"releases" "http://artifactory.local.twitter.com/libs-releases-local"
                  "snapshots" "http://artifactory.local.twitter.com/libs-snapshots-local"}
-;;   :dependencies [[
-;;                   [org.clojars.paul/core-kafka_2.8.0 "0.7.0-1"
-;;                    :exclusions
-;;                    [javax.mail/mail
-;;                     javax.jms/jms
-;;                     com.sun.jdmk/jmxtools
-;;                     com.sun.jmx/jmxri
-;;                     jline/jline
-;;                     net.sf.jopt-simple/jopt-simple
-;;                     junit/junit]]
-;;                   [org.scala-lang/scala-library "2.8.0"]
-;;                   org.apache.zookeeper/zookeeper
-;;                   log4j/log4j
-;;                   ]
-;;                  ]
-  ;; ]
 
 
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojars.paul/core-kafka_2.8.0 "0.7.0-1"
+  :dependencies [
+                 [storm/kafka "0.7.0-incubating"
                   :exclusions
-                  [javax.mail/mail
-                   javax.jms/jms
-                   com.sun.jdmk/jmxtools
-                   com.sun.jmx/jmxri
-                   jline/jline
-                   net.sf.jopt-simple/jopt-simple
-                   junit/junit]]
+                  [org.apache.zookeeper/zookeeper
+                   log4j/log4j
+                   ]
+                  ]
                  [org.scala-lang/scala-library "2.8.0"]
-                 [zookeeper-clj "0.9.2"]]
+
+                 ]
+
+
+
 
   :dev-dependencies [[storm "0.8.2"]
                      [org.clojure/clojure "1.4.0"]]
