e85a43bd9a046aa79c725cece4cf8dde92d94f3b
jiahong.ljh <jiahong.ljh@alibaba-inc.com>
2014-05-14 19:40:05 +0800

    ship with dep com.netflex.curator/curator-framework 1.3.3

--- storm-kafka/project.clj
+++ storm-kafka/project.clj
@@ -3,11 +3,14 @@
   :repositories {"scala-tools" "http://scala-tools.org/repo-releases"
                   "conjars" "http://conjars.org/repo/"}
   :dependencies [[org.scala-lang/scala-library "2.9.2"]
+                 [com.netflix.curator/curator-framework "1.3.3"
+                  :exclusions [log4j/log4j
+                               org.slf4j/slf4j-log4j12]]
                   [com.twitter/kafka_2.9.2 "0.7.0"
                   :exclusions [org.apache.zookeeper/zookeeper
                                log4j/log4j]]]
   :profiles
-  {:provided {:dependencies [[org.apache.storm/storm-core "0.9.2-incubating"]
+  {:provided {:dependencies [[org.apache.storm/storm-core "0.9.2-incubating-SNAPSHOT"]
                              [org.slf4j/log4j-over-slf4j "1.6.6"]
                              ;;[ch.qos.logback/logback-classic "1.0.6"]
                              [org.clojure/clojure "1.4.0"]]}}
